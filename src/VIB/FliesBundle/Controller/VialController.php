f85fa4ba6286537fabbbf95056e33dac12a2aba7
u0078517 <u0078517@192.168.0.196>
2013-02-15 21:51:40 +0100

    Controller refactoring, template optimization

--- src/VIB/FliesBundle/Controller/VialController.php
+++ src/VIB/FliesBundle/Controller/VialController.php
@@ -25,8 +25,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\QueryBuilder;
 
 use VIB\FliesBundle\Form\SelectType;
-use VIB\FliesBundle\Helpers\PDFLabel;
+use VIB\FliesBundle\Utils\PDFLabel;
 use VIB\FliesBundle\Entity\FlyVial;
+use VIB\FliesBundle\Entity\FlyCross;
+
+use \DateTime;
+use \DateInterval;
 
 /**
  * VialController class
@@ -37,24 +41,51 @@ abstract class VialController extends CRUDController {
     
     /**
      * {@inheritdoc}
+     * 
      * @return array|Symfony\Component\HttpFoundation\Response
      */
     protected function getListResponse($page = 1, QueryBuilder $query = null, $maxPerPage = 15)
     {        
         $response = parent::getListResponse($page,$query,$maxPerPage);
-        $formResponse = $this->handleSelectForm(new SelectType($this->getEntityClass()));
+        $formResponse = $this->handleSelectForm(new SelectType('VIB\FliesBundle\Entity\FlyVial'));
         
         return is_array($formResponse) ? array_merge($response, $formResponse) : $formResponse;
     }
     
     /**
+     * Get default batch action response
+     * 
+     * @return Symfony\Component\HttpFoundation\Response
+     */
+    protected abstract function getDefaultBatchResponse();
+    
+    /**
      * Handle batch action
      * 
-     * @param string $action
      * @param array $data
      * @return Symfony\Component\HttpFoundation\Response
      */
-    public abstract function handleBatchAction($data);
+    public function handleBatchAction($data) {
+        
+        $action = $data['action'];
+        $vials = $data['items'];
+        
+        $response = $this->getDefaultBatchResponse();
+        
+        switch($action) {
+            case 'label':
+                $response = $this->generateLabels($vials);
+                break;
+            case 'flip':
+                $response = $this->flipVials($vials);
+                break;
+            case 'trash':
+                $response = $this->trashVials($vials);
+                break;
+        }
+        
+        return $response;
+    }
     
     /**
      * Handle vial selection form
@@ -88,7 +119,7 @@ abstract class VialController extends CRUDController {
     public function generateLabels(Collection $vials) {
         
         $em = $this->getDoctrine()->getManager();
-        $pdf = new PDFLabel();
+        $pdf = new PDFLabel($this->get('white_october.tcpdf'));
         
         foreach ($vials as $vial) {
             $vial->setLabelPrinted(true);
@@ -111,18 +142,32 @@ abstract class VialController extends CRUDController {
         $em = $this->getDoctrine()->getManager();
 
         $newvials = new ArrayCollection();
+        $newcrosses = new ArrayCollection();
         
         foreach ($vials as $vial) {       
-            $newvial = new FlyVial($vial);
-            $newvials->add($newvial);
-            $em->persist($newvial);
+            if (null !== $vial->getCross()) {
+                $newcross = new FlyCross($vial->getCross(),true);
+                $newcrosses->add($newcross);
+                $newvials->add($newcross->getVial());
+                $em->persist($newcross);
+            } else {
+                $newvial = new FlyVial($vial);
+                $newvials->add($newvial);
+                $em->persist($newvial);
+            }
         }
         
         $em->flush();
         
         foreach ($newvials as $vial) {
-            $this->setACL($vial);
+            parent::setACL($vial);
         }
+        
+        foreach ($newcrosses as $cross) {
+            parent::setACL($cross);
+        }
+        
+        return $this->getDefaultBatchResponse();
     }
     
     /**
@@ -139,6 +184,8 @@ abstract class VialController extends CRUDController {
             $em->persist($vial);
         }
         
-        $em->flush();        
+        $em->flush();
+        
+        return $this->getDefaultBatchResponse();
     }
 }
--- src/VIB/FliesBundle/Form/FlyVialExpandType.php
+++ src/VIB/FliesBundle/Form/FlyVialExpandType.php
@@ -1,42 +1,59 @@
 <?php
 
+/*
+ * Copyright 2011 Radoslaw Kamil Ejsmont <radoslaw@ejsmont.net>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 namespace VIB\FliesBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 
 use Symfony\Component\Validator\Constraints\Min;
 use Symfony\Component\Validator\Constraints\NotNull;
-use Symfony\Component\Validator\Constraints\Collection;
 
+/**
+ * FlyVialExpandType class
+ *
+ * @author Radoslaw Kamil Ejsmont <radoslaw@ejsmont.net>
+ */
 class FlyVialExpandType extends AbstractType
 {
+    /**
+     * {@inheritdoc}
+     */
     public function getName()
     {
-        return "FlyVialExpandType";
+        return "flyvial_expand";
     }
     
-    public function buildForm(FormBuilder $builder, array $options)
-    {
-        $builder->add('source', 'null_entity', array(
-                        'property'     => 'id',
-                        'class' => 'VIBFliesBundle:FlyVial',
-                        'required' => true,
-                        'hidden'    => true,
-                        'error_bubbling' => false ))
-                ->add('number','number', array('label' => 'Number of vials'));
-    }
-
-    public function getDefaultOptions(array $options)
-    {
-        $collectionConstraint = new Collection(array(
-            'source' => new NotNull(),
-            'number' => new Min(1),
-        ));
-                
-        return array(
-            'validation_constraint' => $collectionConstraint
-        );
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)    {
+        $builder->add('source', 'text_entity', array(
+                        'property' => 'id',
+                        'class'    => 'VIBFliesBundle:FlyVial',
+                        'format'   => '%06d',
+                        'label'    => 'Source',
+                        'constraints' => array(
+                            new NotNull())))
+                ->add('number','number', array(
+                        'label'       => 'Number of vials',
+                        'constraints' => array(
+                            new Min(1))));
     }
 }
 
