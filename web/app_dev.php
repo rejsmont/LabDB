5ff95eedd44a8bcefa1ac158bf3e10ad0e55b860
Radoslaw Ejsmont <radoslaw@ejsmont.net>
2013-03-14 10:34:35 +0100

    Case corrected

--- web/app_dev.php
+++ web/app_dev.php
@@ -11,7 +11,8 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
         '127.0.0.1',
         '::1',
-        '10.35.70.59'
+        '10.35.70.59',
+        '10.33.237.244'
     ))
 ) {
     header('HTTP/1.0 403 Forbidden');
