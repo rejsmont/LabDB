de67e6695cc2d5c1d2611ed8fa784fa11efdf83f
rejsmont <radoslaw@ejsmont.net>
2013-04-12 13:10:48 +0200

    WelcomeBundle is now PSR-2 compatible

--- src/VIB/WelcomeBundle/DependencyInjection/VIBWelcomeExtension.php
+++ src/VIB/WelcomeBundle/DependencyInjection/VIBWelcomeExtension.php
@@ -1,5 +1,21 @@
 <?php
 
+/*
+ * Copyright 2013 Radoslaw Kamil Ejsmont <radoslaw@ejsmont.net>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 namespace VIB\WelcomeBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -7,10 +23,11 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
+
 /**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ * VIBSecurityExtension
+ * 
+ * @author Radoslaw Kamil Ejsmont <radoslaw@ejsmont.net>
  */
 class VIBWelcomeExtension extends Extension
 {
@@ -26,3 +43,5 @@ class VIBWelcomeExtension extends Extension
         $loader->load('services.xml');
     }
 }
+
+?>
